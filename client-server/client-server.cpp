e5d6649a4172713a575dcc8013eaaf047202f27e
Roout <following2future@gmail.com>
2018-09-24 10:11:39 +0300

    Add project files.

---
+++ client-server/client-server.cpp
@@ -0,0 +1,12 @@
+﻿// client-server.cpp : Defines the entry point for the application.
+//
+
+#include "client-server.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
