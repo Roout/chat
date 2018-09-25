ee46b90a8ab201c3f18e46ceea660a87dd7f6168
Roout <following2future@gmail.com>
2018-09-25 23:41:20 +0300

    Add simple test for boost

--- client-server/main.cpp
+++ client-server/main.cpp
@@ -1,15 +1,31 @@
 // client-server.cpp : Defines the entry point for the application.
 //
-
 #include "client-server.h"
-#include <boost/lexical_cast.hpp>
+#include <boost/asio.hpp>
+#include <boost/chrono.hpp>
+#include <functional>
+#include <thread>
 using namespace std;
+
+void TimerHandler(boost::asio::deadline_timer& timer, int time) {
+	if (!time)
+		cout << "Finish!\n";
+	else {
+		cout << ".." << time << endl;
+		timer.expires_from_now(boost::posix_time::seconds(1));
+		timer.async_wait(bind(&TimerHandler,ref(timer),--time));
+	}
+}
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	cin.get();
+	boost::asio::io_context ioc;
+	boost::asio::deadline_timer timer(ioc);
+	boost::asio::post(ioc, bind(&TimerHandler, ref(timer), 3));
+	thread t([&]() {
+		ioc.run();
+	});
+	t.join();
 
-	cout << boost::lexical_cast<int>("1233");
 	return 0;
 }
