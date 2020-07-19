687805f0d17212118ea1251cc82c6c0e6aef0a68
Roout <following2future@gmail.com>
2020-07-19 17:38:09 +0300

    add utilities for the work with enumerations.

---
+++ client-server/include/Utility.hpp
@@ -0,0 +1,25 @@
+#ifndef LOCAL_UTILITY_HPP
+#define LOCAL_UTILITY_HPP
+
+
+namespace Util {
+    /**
+     * Work with enumerations: 
+     */
+    template<class Enum>
+    constexpr size_t EnumCast(Enum value) noexcept {
+        return static_cast<size_t>(value);
+    }
+
+    template<class Enum>
+    constexpr size_t EnumSize() noexcept {
+        return EnumCast(Enum::COUNT);
+    } 
+
+    template<class Enum>
+    constexpr Enum EnumCast(size_t value) noexcept {
+        return static_cast<Enum>(value);
+    }
+}
+
+#endif // LOCAL_UTILITY_HPP
