1f5dd41e70babbc60a43d8868e8bd7ec6ee08b93
Roout <following2future@gmail.com>
2020-07-25 23:48:36 +0300

    adjust tests to error code which is indicator of the Request::Parse success.

--- tests/request-tests.hpp
+++ tests/request-tests.hpp
@@ -6,14 +6,16 @@ TEST(Request, ParseLeaveChatroomRequest) {
     Requests::Request request;
     // form test
     const std::string frame = 
-        std::to_string(Util::EnumCast(Requests::RequestType::LEAVE_CHATROOM)) // command
+        std::to_string(Utils::EnumCast(Requests::RequestType::LEAVE_CHATROOM)) // command
         + Requests::DELIMETER + "0"  // chatroom id
         + Requests::DELIMETER + "Roster557" // unique username
         + Requests::DELIMETER + "" // body
         + Requests::REQUEST_DELIMETER; 
     // parse
-    request.Parse(frame);
+    const auto result = request.Parse(frame);
     // check
+    const auto successCode { 0 };
+    ASSERT_EQ(result, successCode);
     ASSERT_EQ(request.GetType(), Requests::RequestType::LEAVE_CHATROOM);
     ASSERT_EQ(request.GetChatroom(), 0);
     ASSERT_EQ(request.GetName(), std::string("Roster557"));
@@ -24,14 +26,16 @@ TEST(Request, ParseJoinChatroomRequest) {
     Requests::Request request;
     // form test
     const std::string frame = 
-        std::to_string(Util::EnumCast(Requests::RequestType::JOIN_CHATROOM)) // command
+        std::to_string(Utils::EnumCast(Requests::RequestType::JOIN_CHATROOM)) // command
         + Requests::DELIMETER + "0"  // chatroom id
         + Requests::DELIMETER + "Roster557" // unique username
         + Requests::DELIMETER + "" // body
         + Requests::REQUEST_DELIMETER; 
     // parse
-    request.Parse(frame);
+    const auto result = request.Parse(frame);
     // check
+    const auto successCode { 0 };
+    ASSERT_EQ(result, successCode);
     ASSERT_EQ(request.GetType(), Requests::RequestType::JOIN_CHATROOM);
     ASSERT_EQ(request.GetChatroom(), 0);
     ASSERT_EQ(request.GetName(), std::string("Roster557"));
@@ -42,14 +46,16 @@ TEST(Request, ParseAboutChatroomRequest) {
     Requests::Request request;
     // form test
     const std::string frame = 
-        std::to_string(Util::EnumCast(Requests::RequestType::ABOUT_CHATROOM)) // command
+        std::to_string(Utils::EnumCast(Requests::RequestType::ABOUT_CHATROOM)) // command
         + Requests::DELIMETER + "134"       // chatroom id
         + Requests::DELIMETER + "Roster557" // unique username
         + Requests::DELIMETER + "6:some other info"   // body
         + Requests::REQUEST_DELIMETER; 
     // parse
-    request.Parse(frame);
+    const auto result = request.Parse(frame);
     // check
+    const auto successCode { 0 };
+    ASSERT_EQ(result, successCode);
     ASSERT_EQ(request.GetType(), Requests::RequestType::ABOUT_CHATROOM);
     ASSERT_EQ(request.GetChatroom(), 134);
     ASSERT_EQ(request.GetName(), std::string{"Roster557"});
@@ -60,14 +66,16 @@ TEST(Request, ParseCreateChatroomRequest) {
     Requests::Request request;
     // form test
     const std::string frame = 
-        std::to_string(Util::EnumCast(Requests::RequestType::CREATE_CHATROOM)) // command
+        std::to_string(Utils::EnumCast(Requests::RequestType::CREATE_CHATROOM)) // command
         + Requests::DELIMETER + "0"       // chatroom id
         + Requests::DELIMETER + "Roster557" // unique username
         + Requests::DELIMETER + "Only for girls!"   // body
         + Requests::REQUEST_DELIMETER; 
     // parse
-    request.Parse(frame);
+    const auto result = request.Parse(frame);
     // check
+    const auto successCode { 0 };
+    ASSERT_EQ(result, successCode);
     ASSERT_EQ(request.GetType(), Requests::RequestType::CREATE_CHATROOM);
     ASSERT_EQ(request.GetChatroom(), 0);
     ASSERT_EQ(request.GetName(), std::string{"Roster557"});
@@ -78,14 +86,16 @@ TEST(Request, ParseListChatroomRequest) {
     Requests::Request request;
     // form test
     const std::string frame = 
-        std::to_string(Util::EnumCast(Requests::RequestType::LIST_CHATROOM)) // command
+        std::to_string(Utils::EnumCast(Requests::RequestType::LIST_CHATROOM)) // command
         + Requests::DELIMETER + "0"       // chatroom id
         + Requests::DELIMETER + "Roster557" // unique username
         + Requests::DELIMETER + ""   // body
         + Requests::REQUEST_DELIMETER; 
     // parse
-    request.Parse(frame);
+    const auto result = request.Parse(frame);
     // check
+    const auto successCode { 0 };
+    ASSERT_EQ(result, successCode);
     ASSERT_EQ(request.GetType(), Requests::RequestType::LIST_CHATROOM);
     ASSERT_EQ(request.GetChatroom(), 0);
     ASSERT_EQ(request.GetName(), std::string{"Roster557"});
@@ -96,14 +106,16 @@ TEST(Request, ParseAuthorizeRequest) {
     Requests::Request request;
     // form test
     const std::string frame = 
-        std::to_string(Util::EnumCast(Requests::RequestType::AUTHORIZE)) // command
+        std::to_string(Utils::EnumCast(Requests::RequestType::AUTHORIZE)) // command
         + Requests::DELIMETER + "0"       // chatroom id
         + Requests::DELIMETER + "Roster557" // unique username
         + Requests::DELIMETER + ""   // body; 
         + Requests::REQUEST_DELIMETER; 
     // parse
-    request.Parse(frame);
+    const auto result = request.Parse(frame);
     // check
+    const auto successCode { 0 };
+    ASSERT_EQ(result, successCode);
     ASSERT_EQ(request.GetType(), Requests::RequestType::AUTHORIZE);
     ASSERT_EQ(request.GetChatroom(), 0);
     ASSERT_EQ(request.GetName(), std::string{"Roster557"});
@@ -114,14 +126,16 @@ TEST(Request, ParsePostRequest) {
     Requests::Request request;
     // form test
     const std::string frame = 
-        std::to_string(Util::EnumCast(Requests::RequestType::POST)) // command
+        std::to_string(Utils::EnumCast(Requests::RequestType::POST)) // command
         + Requests::DELIMETER + "0" // chatroom id
         + Requests::DELIMETER + ""  // unique username
         + Requests::DELIMETER + "#1 Friends from Bruklin\n#2 Friends from Tokio"  // body; 
         + Requests::REQUEST_DELIMETER; 
     // parse
-    request.Parse(frame);
+    const auto result = request.Parse(frame);
     // check
+    const auto successCode { 0 };
+    ASSERT_EQ(result, successCode);
     ASSERT_EQ(request.GetType(), Requests::RequestType::POST);
     ASSERT_EQ(request.GetChatroom(), 0);
     ASSERT_EQ(request.GetName(), std::string{""});
@@ -132,14 +146,16 @@ TEST(Request, ParseChatMessageRequest) {
     Requests::Request request;
     // form test
     const std::string frame = 
-        std::to_string(Util::EnumCast(Requests::RequestType::CHAT_MESSAGE)) // command
+        std::to_string(Utils::EnumCast(Requests::RequestType::CHAT_MESSAGE)) // command
         + Requests::DELIMETER + "0"       // chatroom id
         + Requests::DELIMETER + "Roster557" // unique username
         + Requests::DELIMETER + ""   // body
         + Requests::REQUEST_DELIMETER; 
     // parse
-    request.Parse(frame);
+    const auto result = request.Parse(frame);
     // check
+    const auto successCode { 0 };
+    ASSERT_EQ(result, successCode);
     ASSERT_EQ(request.GetType(), Requests::RequestType::CHAT_MESSAGE);
     ASSERT_EQ(request.GetChatroom(), 0);
     ASSERT_EQ(request.GetName(), std::string{"Roster557"});
@@ -150,15 +166,16 @@ TEST(Request, SerializeRequest) {
     Requests::Request request;
     // form test
     const std::string frame = 
-        std::to_string(Util::EnumCast(Requests::RequestType::ABOUT_CHATROOM)) // command
+        std::to_string(Utils::EnumCast(Requests::RequestType::ABOUT_CHATROOM)) // command
         + Requests::DELIMETER + "134"       // chatroom id
         + Requests::DELIMETER + "Roster557" // unique username
         + Requests::DELIMETER + "6:some other info"   // body
         + Requests::REQUEST_DELIMETER; 
     // parse
-    request.Parse(frame);
-
+    const auto result = request.Parse(frame);
+    const auto successCode { 0 };
+    ASSERT_EQ(result, successCode);
+    
     const auto serialized { request.Serialize() };
-
     ASSERT_EQ(serialized, frame);
 }
