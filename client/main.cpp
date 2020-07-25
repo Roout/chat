176b027b61f1f66ee1d4cc5dc253d6b150786af0
Roout <following2future@gmail.com>
2020-07-25 23:36:23 +0300

    * fix bug: absence of thread joining; * wrap in try-catch io_context::run method called in threads.

--- client/main.cpp
+++ client/main.cpp
@@ -3,7 +3,7 @@
 #include <boost/asio.hpp>
 #include <thread>
 #include <vector>
-#include "client.hpp"
+#include "Client.hpp"
 using namespace std;
 
 int main() {
@@ -14,10 +14,19 @@ int main() {
 	Client client { io };
 	client.Connect("127.0.0.1", 15001);
 	vector<thread> ts;
-	for(int i = 0; i < 2; i++) 
+	for(int i = 0; i < 2; i++) {
 		ts.emplace_back([io]() {
-			io->run(); 
+			for (;;) {
+				try {
+					io->run();
+					break; // run() exited normally
+				}
+				catch (...) {
+					// Deal with exception as appropriate.
+				}
+			}
 		});
+	}
 	
 	cin.ignore();
 
@@ -25,6 +34,10 @@ int main() {
 		string str;
 		getline(cin, str);
 		client.Write(std::move(str));
+	}
+
+	for(auto& t: ts) {
+		t.join();
 	}
 	return 0;
 }
