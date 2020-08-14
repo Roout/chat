d3ac8dd231c6fe5406da2f793919e527ede049fb
Roout <following2future@gmail.com>
2020-08-14 10:31:34 +0300

    Add test for unauthorized access to chattooms list data.

--- tests/session-tests.hpp
+++ tests/session-tests.hpp
@@ -67,7 +67,7 @@ protected:
 TEST_F(TCPInteractionTest, OnlyFixureSetup) {
     // prepare (done in ::SetUp method)
     // execute (done in ::SetUp method)
-    this->WaitFor(3'000);
+    this->WaitFor(25);
     // test
     EXPECT_EQ(m_client->GetStage(), IStage::State::UNAUTHORIZED) 
         << "Problems with Fixure initialization occured: "
@@ -100,16 +100,16 @@ TEST_F(TCPInteractionTest, ClientAuthorization) {
 }
 
 TEST_F(TCPInteractionTest, AuthorizedRequestChatroomList) {
-    // TODO: 1. Server creates chatrooms
+    /// 1. Server creates chatrooms
     m_server->CreateChatroom("WoW 3.3.5a");
     m_server->CreateChatroom("Dota 2");
     m_server->CreateChatroom("Programming");
     m_server->CreateChatroom("Chatting");
-    // 2. Complete authorization 
+    
+    /// 2. Complete authorization 
     Requests::Request request{};
     request.SetType(Requests::RequestType::AUTHORIZE);
     request.SetName("Random Name #1");
-    request.SetBody("Testing authorization");
     
     // send authorization request to server
     m_client->Write(request.Serialize());   
@@ -121,19 +121,17 @@ TEST_F(TCPInteractionTest, AuthorizedRequestChatroomList) {
         << "\nClient's stage is: " << static_cast<size_t>(m_client->GetStage())
         << "\nExpected: " << static_cast<size_t>(IStage::State::AUTHORIZED); 
     
-    // 3. Request chatroom list
+    /// 3. Request chatroom list
     Requests::Request listRequest{};
-    request.SetType(Requests::RequestType::LIST_CHATROOM);
-    request.SetName("Random Name #1");
-    request.SetBody("Testing chatroom list");
+    listRequest.SetType(Requests::RequestType::LIST_CHATROOM);
     // send request to server
-    m_client->Write(request.Serialize());   
-    // give  0.025 seconds for server - client communication
+    m_client->Write(listRequest.Serialize());   
+    // give  0.025 seconds for server response
     this->WaitFor(25);
 
-    ///TODO: 4. Compare expected result and recieved response
+    /// 4. Compare expected result and recieved response
     const auto sourceChatList = m_server->GetChatroomList(); // chatroom list which has been sent
-    const auto aquiredChatList = m_client->GetGUI().GetBuffer(); // chatroom list which has been recieved united via '\n' symbol
+    const auto aquiredChatList = m_client->GetGUI().GetRequest().GetBody(); // chatroom list which has been recieved united via '\n' symbol
     // devide chatroom list
     std::vector<std::string> resultList;
     size_t start { 0 };
@@ -146,6 +144,7 @@ TEST_F(TCPInteractionTest, AuthorizedRequestChatroomList) {
     resultList.emplace_back(aquiredChatList.substr(start, std::string::npos));
 
     // compare source and result
+    EXPECT_EQ(m_client->GetGUI().GetRequest().GetCode(), Requests::ErrorCode::SUCCESS);
     EXPECT_EQ(resultList.size(), sourceChatList.size());
     for(size_t i = 0; i < resultList.size(); i++) {
         EXPECT_EQ(resultList[i], sourceChatList[i]);
@@ -153,5 +152,31 @@ TEST_F(TCPInteractionTest, AuthorizedRequestChatroomList) {
 }
 
 TEST_F(TCPInteractionTest, UnauthorizedRequestChatroomList) {
+    /// 1. Server creates chatrooms
+    m_server->CreateChatroom("WoW 3.3.5a");
+    m_server->CreateChatroom("Dota 2");
+    m_server->CreateChatroom("Programming");
+    m_server->CreateChatroom("Chatting");
+    
+    /// 2. Ignore authorization 
+    EXPECT_EQ(m_client->GetStage(), IStage::State::UNAUTHORIZED) 
+        << "Problems with Fixure initialization occured: "
+        << "\nClient's stage is: " << static_cast<size_t>(m_client->GetStage())
+        << "\nExpected: " << static_cast<size_t>(IStage::State::UNAUTHORIZED); 
+
+    /// 3. Request chatroom list
+    Requests::Request listRequest{};
+    listRequest.SetType(Requests::RequestType::LIST_CHATROOM);
+    // send request to server
+    m_client->Write(listRequest.Serialize());   
+    // give  0.025 seconds for server response
+    this->WaitFor(25);
+    /// 4. Compare expected result and recieved response
+    const auto sourceChatList = m_server->GetChatroomList(); // chatroom list which has been sent
+    const auto &recievedReqeuest = m_client->GetGUI().GetRequest();
+    
+    EXPECT_EQ(recievedReqeuest.GetType(), Requests::RequestType::LIST_CHATROOM);
+    EXPECT_EQ(recievedReqeuest.GetStage(), IStage::State::UNAUTHORIZED);
+    EXPECT_EQ(recievedReqeuest.GetCode(), Requests::ErrorCode::FAILURE);
 
 }
