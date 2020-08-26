fc32bb2d4612c86c40505a8aec0f3cdc602809b7
Roout <following2future@gmail.com>
2020-08-26 12:16:49 +0300

    test(message): adapt tests to request-response model

--- tests/message-tests.hpp
+++ tests/message-tests.hpp
@@ -9,30 +9,25 @@
 TEST(RequestTest, ParseLeaveChatroomRequest) {
     const std::string requestStr = R"(
        {
-            "tag": "request",
-            "protocol": "RRP",
-            "type": "leave-chatroom",
+            "query": "leave-chatroom",
             "timestamp": 344678435900,
             "timeout": 30
         }
     )";  
     
-    auto message = Internal::Read(requestStr);
-    auto request = dynamic_cast<Internal::Request*>(message.get());
+    Internal::Request request{};
+    request.Read(requestStr);
 
-    EXPECT_NE(request, nullptr);
-    EXPECT_EQ(request->m_timeout, 30);
-    EXPECT_EQ(request->m_type, Internal::QueryType::LEAVE_CHATROOM);
-    EXPECT_EQ(request->m_timestamp, 344678435900LL);
-    EXPECT_EQ(request->m_attachment, std::string(""));
+    EXPECT_EQ(request.m_timeout, 30);
+    EXPECT_EQ(request.m_query, Internal::QueryType::LEAVE_CHATROOM);
+    EXPECT_EQ(request.m_timestamp, 344678435900LL);
+    EXPECT_TRUE(request.m_attachment.empty());
 }
 
 TEST(RequestTest, ParseJoinChatroomRequest) {
     const std::string requestStr = R"(
        {
-            "tag": "request",
-            "protocol": "RRP",
-            "type": "join-chatroom",
+            "query": "join-chatroom",
             "timestamp": 344678435232,
             "timeout": 30,
             "body": {
@@ -46,22 +41,19 @@ TEST(RequestTest, ParseJoinChatroomRequest) {
         }
     )";  
    
-    auto message = Internal::Read(requestStr);
-    auto request = dynamic_cast<Internal::Request*>(message.get());
+    Internal::Request request{};
+    request.Read(requestStr);
 
-    EXPECT_NE(request, nullptr);
-    EXPECT_EQ(request->m_timeout, 30);
-    EXPECT_EQ(request->m_type, Internal::QueryType::JOIN_CHATROOM);
-    EXPECT_EQ(request->m_timestamp, 344678435232LL);
-    EXPECT_FALSE(request->m_attachment.empty());
+    EXPECT_EQ(request.m_timeout, 30);
+    EXPECT_EQ(request.m_query, Internal::QueryType::JOIN_CHATROOM);
+    EXPECT_EQ(request.m_timestamp, 344678435232LL);
+    EXPECT_FALSE(request.m_attachment.empty());
 }
 
 TEST(RequestTest, ParseCreateChatroomRequest) {
     const std::string requestStr = R"(
        {
-            "tag": "request",
-            "protocol": "RRP",
-            "type": "create-chatroom",
+            "query": "create-chatroom",
             "timestamp": 344678435232,
             "timeout": 30,
             "body":{
@@ -75,35 +67,31 @@ TEST(RequestTest, ParseCreateChatroomRequest) {
         }
     )";  
    
-    auto message = Internal::Read(requestStr);
-    auto request = dynamic_cast<Internal::Request*>(message.get());
+    Internal::Request request{};
+    request.Read(requestStr);
 
-    EXPECT_NE(request, nullptr);
-    EXPECT_EQ(request->m_timeout, 30);
-    EXPECT_EQ(request->m_type, Internal::QueryType::CREATE_CHATROOM);
-    EXPECT_EQ(request->m_timestamp, 344678435232LL);
-    EXPECT_FALSE(request->m_attachment.empty());
+    EXPECT_EQ(request.m_timeout, 30);
+    EXPECT_EQ(request.m_query, Internal::QueryType::CREATE_CHATROOM);
+    EXPECT_EQ(request.m_timestamp, 344678435232LL);
+    EXPECT_FALSE(request.m_attachment.empty());
 }
 
 TEST(RequestTest, ParseListChatroomRequest) {
     const std::string requestStr = R"(
        {
-            "tag": "request",
-            "protocol": "RRP",
-            "type": "list-chatroom",
+            "query": "list-chatroom",
             "timestamp": 344678435266,
             "timeout": 30
         }
     )";  
    
-    auto message = Internal::Read(requestStr);
-    auto request = dynamic_cast<Internal::Request*>(message.get());
-
-    EXPECT_NE(request, nullptr);
-    EXPECT_EQ(request->m_timeout, 30);
-    EXPECT_EQ(request->m_type, Internal::QueryType::LIST_CHATROOM);
-    EXPECT_EQ(request->m_timestamp, 344678435266LL);
-    EXPECT_TRUE(request->m_attachment.empty());
+    Internal::Request request{};
+    request.Read(requestStr);
+   
+    EXPECT_EQ(request.m_timeout, 30);
+    EXPECT_EQ(request.m_query, Internal::QueryType::LIST_CHATROOM);
+    EXPECT_EQ(request.m_timestamp, 344678435266LL);
+    EXPECT_TRUE(request.m_attachment.empty());
 }
 
 // ========== Response ========= //
@@ -138,18 +126,6 @@ TEST(ResponseTest, BuildCreateChatroomResponse) {
 
 TEST(ResponseTest, BuildListChatroomResponse) {
     
-}
-
-// ============ Chat ============ //
-
-TEST(ChatTest, BuildChatMessage) {
-    Internal::Request chat {};
-   
-}
-
-TEST(ChatTest, ParseChatMessage) {
-    std::string json = "";
-   
 }
 
 #endif // REQUEST_TESTS_HPP
