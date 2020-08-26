9d866cf0a2d919ebb25893ae909029b8e1288309
Roout <following2future@gmail.com>
2020-08-26 10:17:56 +0300

    refactor(User): make private user's constructor
    Change access modificator for constructor to forbid creating users with the custom ID. It exist only for creating instance by parsing serialized user.

--- classes/User.hpp
+++ classes/User.hpp
@@ -7,25 +7,21 @@ namespace Internal {
 
     struct User final {
         // Lifetime management
-
         User();
 
-        User(std::size_t id);
-
         // Methods
-
         std::string AsJSON() const;
 
         static User FromJSON(const std::string& json);
-
-    public:
+      
         // Properties
-
         const std::size_t m_id;
         std::size_t m_chatroom;
         std::string m_username;
 
     private:
+        User(std::size_t id);
+
         inline static std::size_t m_instance { 0 }; 
     };
 }
