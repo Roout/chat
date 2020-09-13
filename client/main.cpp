2e30fab90f5fb4c1b1ca96e4f1374e06b28f87bb
Roout <following2future@gmail.com>
2020-09-13 11:37:57 +0300

    fix(Client): create client instance with shared pointer
    changed because of client's  inheritance from `enable_shared_from_this`

--- client/main.cpp
+++ client/main.cpp
@@ -11,8 +11,8 @@ int main() {
 		std::make_shared<boost::asio::io_context>() 
 	};
 	
-	Client client { io };
-	client.Connect("127.0.0.1", 15001);
+	auto client = std::make_shared<Client>(io);
+	client->Connect("127.0.0.1", 15001);
 	vector<thread> ts;
 	for(int i = 0; i < 2; i++) {
 		ts.emplace_back([io]() {
@@ -33,7 +33,7 @@ int main() {
 	while(true) {
 		string str;
 		getline(cin, str);
-		client.Write(std::move(str));
+		client->Write(std::move(str));
 	}
 
 	for(auto& t: ts) {
